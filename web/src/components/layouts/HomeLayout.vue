c026f84fc374bfcb26ef61a7cf9a8bde6f66772a
chenlt-qtl <da.mu@163.com>
2019-04-29 10:19:16 +0800

    增加homeLayout.

---
+++ web/src/components/layouts/HomeLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <a-layout class="layout" :class="[device]">
+
+    <a-layout
+      :class="[layoutMode, `content-width-${contentWidth}`]"
+      :style="{ paddingLeft: fixSiderbar && isDesktop() ? `${sidebarOpened ? 200 : 80}px` : '0' }">
+      <!-- layout header -->
+      <home-header
+        :mode="layoutMode"
+        :menus="menus"
+        :theme="navTheme"
+        :collapsed="collapsed"
+        :device="device"
+        @toggle="toggle"
+      />
+
+      <!-- layout content -->
+      <a-layout-content :style="{ height: '100%', paddingTop: fixedHeader ? '59px' : '0' }">
+        <router-view />
+      </a-layout-content>
+    </a-layout>
+
+    <setting-drawer></setting-drawer>
+  </a-layout>
+</template>
+
+<script>
+  import HomeHeader from '@/components/page/HomeHeader'
+  export default {
+    name: "HomeLayout",
+    components: {
+      HomeHeader
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
