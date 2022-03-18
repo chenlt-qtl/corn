fa59e7cd598b9c377d8a7787071d56c54f0a5e0b
chenlt <da.mu@163.com>
2022-03-18 17:54:03 +0800

    增加card1

---
+++ app/src/pages/exam/components/card1/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './styles.less'
+import 'font-awesome/css/font-awesome.min.css';
+
+
+
+const Card1: React.FC<{}> = () => {
+    return (
+
+        <div className={styles.container}>
+            
+        </div>
+
+    );
+};
+
+export default Card1;
