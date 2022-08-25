50b9f7cbd4b49b5d848e10757678be21940789a0
chenlt <da.mu@163.com>
2022-08-25 14:08:51 +0800

    修改目录刷新问题，修改MD样式

--- app/src/pages/note/Content/MarkDown/index.tsx
+++ app/src/pages/note/Content/MarkDown/index.tsx
@@ -18,29 +18,6 @@ import Tocify from './Tocify';
 
 let tocify = new Tocify();
 
-/**
- * 解析md
- */
-const renderer = new marked.Renderer();
-renderer.heading = function (text, level, raw) {
-    const anchor = tocify.add(text, level);
-    return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
-};
-
-
-marked.setOptions({
-    renderer,
-    highlight: function (code) {
-        return hljs.highlightAuto(code).value;
-    },
-    gfm: true, // 允许 Git Hub标准的markdown.
-    pedantic: false, // 不纠正原始模型任何的不良行为和错误（默认为false）
-    sanitize: false, // 对输出进行过滤（清理），将忽略任何已经输入的html代码（标签）
-    tables: true, // 允许支持表格语法（该选项要求 gfm 为true）
-    breaks: false, // 允许回车换行（该选项要求 gfm 为true）
-    smartLists: false, // 使用比原生markdown更时髦的列表
-    smartypants: false, // 使用更为时髦的标点
-})
 
 MdEditor.use(Plugins.TabInsert, {
     /**
@@ -98,6 +75,69 @@ const MarkDown = React.forwardRef((props, ref) => {
         }
     }, [props.displayIndex])
 
+    //监听窗口关闭
+    useEffect(() => {
+
+        //关闭时保存数据
+        if (!visible) {
+            let text = "";
+            let isChange = false;
+            if (editKey == "new") {
+                if (value != blankText) {
+                    htmlObjs.push({ text: value })
+                    isChange = true;
+                }
+            }
+            htmlObjs.forEach(item => {
+                if (item.id == editKey) {
+                    if (value != item.text) {
+                        text += value;
+                        isChange = true;
+                    }
+                } else {
+                    text += item.text;
+                }
+                text += "\n";
+            })
+
+
+            if (isChange) {
+                saveContent(text);
+            }
+            setEditKey("");
+        }
+    }, [visible])
+
+
+    const renderer = new marked.Renderer();
+    renderer.heading = function (text, level) {
+        if (!visible) {//如果没有显示弹窗的时候，更新目录
+            const anchor = tocify.add(text, level);
+            return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
+        } else {
+            return `<h${level}>${text}</h${level}>`;
+
+        }
+
+
+    };
+
+
+    marked.setOptions({
+        renderer,
+        highlight: function (code) {
+            return hljs.highlightAuto(code).value;
+        },
+        gfm: true, // 允许 Git Hub标准的markdown.
+        pedantic: false, // 不纠正原始模型任何的不良行为和错误（默认为false）
+        sanitize: false, // 对输出进行过滤（清理），将忽略任何已经输入的html代码（标签）
+        tables: true, // 允许支持表格语法（该选项要求 gfm 为true）
+        breaks: false, // 允许回车换行（该选项要求 gfm 为true）
+        smartLists: false, // 使用比原生markdown更时髦的列表
+        smartypants: false, // 使用更为时髦的标点
+    })
+
+
     const processText = (text: string) => {
 
         tocify.reset();
@@ -134,42 +174,9 @@ const MarkDown = React.forwardRef((props, ref) => {
     }
 
     //保存数据，更新view视图
-    const saveContent = () => {
-        processText(value);
-        props.saveContent(value);
-    }
-
-
-    //view视图保存数据
-    const saveTexts = () => {
-        let text = "";
-        let isChange = false;
-        if (editKey == "new") {
-
-            if (value != blankText) {
-                htmlObjs.push({ text: value })
-                isChange = true;
-            }
-        }
-        htmlObjs.forEach(item => {
-            if (item.id == editKey) {
-                if (value != item.text) {
-                    text += value;
-                    isChange = true;
-                }
-            } else {
-                text += item.text;
-            }
-            text += "\n";
-        })
-
-
-        if (isChange) {
-            processText(text);
-            props.saveContent(text);
-        }
-        setVisible(false);
-        setEditKey("");
+    const saveContent = text => {
+        processText(text);
+        props.saveContent(text);
     }
 
     const handleImageUpload = (file, callback) => {
@@ -215,11 +222,11 @@ const MarkDown = React.forwardRef((props, ref) => {
                         {displayIndex == 1 ?
                             <MdEditor
                                 value={value}
-                                renderHTML={text => marked(text)}
+                                renderHTML={renderHTML}
                                 onChange={handleEditorChange}
                                 onImageUpload={handleImageUpload}
-                                onBlur={saveContent}
-                                style={{height:'100%'}}
+                                onBlur={() => saveContent(value)}
+                                style={{ height: '100%' }}
                                 config={{ view: { menu: true, html: isMobile ? false : true, md: true } }}
                             />
                             : <div className={styles.md_view}>
@@ -238,17 +245,16 @@ const MarkDown = React.forwardRef((props, ref) => {
                     closable={false}
                     width={1000}
                     maskClosable={true}
-                    onCancel={saveTexts}
+                    onCancel={() => setVisible(false)}
                 >
                     <div className={styles.md_md}>
                         <MdEditor
                             value={value}
-                            renderHTML={renderHTML}
+                            renderHTML={text => marked(text)}
                             onChange={handleEditorChange}
                             onImageUpload={handleImageUpload}
                             config={{ view: { menu: true, html: true, md: true } }}
-                            style={{ "minHeight": "500px" }}
-                        />
+                            style={{ "minHeight": "500px" }} />
                     </div>
                 </Modal>
             </>
