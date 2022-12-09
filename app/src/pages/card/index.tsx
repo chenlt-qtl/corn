be0bf1a263a1eb3615ed0cf2f6e34478ae60e5a1
chenlt <da.mu@163.com>
2022-12-09 14:59:35 +0800

    完成Card

--- app/src/pages/card/index.tsx
+++ app/src/pages/card/index.tsx
@@ -1,41 +1,24 @@
 import { Modal } from 'antd';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles.less'
-import { getGameList } from '@/services/game'
-import { reduce } from 'lodash';
-
+import { cardItems as itemList} from "@/utils/constants";
+import {cardTabs as tabs } from "@/utils/constants";
 
 const staticData = [3, 2, 1, -1, -2, -3]
-// const itemList = [{ id: 1, title: "a", value: 3 },
-// { id: 2, title: "b", value: -3 }]
+// const itemList = [
+//     [{ title: "a", values: [3, -3] },
+//     { title: "b", value: -3 }],
+//      [{ title: "c", values: [3, -3] },{ title: "d", values: [3, -3] }],
+// ]
 
-const itemList = [
-    { id: 1, title: "认真吃饭", value: 1 },
-    { id: 2, title: "不挑食", value: 1 },
-    { id: 1, title: "洗澡", value: 1 },
-    { id: 1, title: "刷牙", value: 1 },
-    { id: 1, title: "抄作业", value: 2 },
-    { id: 1, title: "7点前完成作业", value: 10 },
-    { id: 1, title: "8点前完成作业", value: 5 },
-    { id: 1, title: "9点前完成作业", value: 2 },
-    { id: 1, title: "9点后完成作业", value: -5 },
-    { id: 1, title: "10点后完成作业", value: -10 },
-    { id: 1, title: "10点前睡觉", value: 5 },
-    { id: 1, title: "睡前不说话", value: 5 },
-    { id: 1, title: "没完成作业前画画", value: -2 },
-    { id: 1, title: "挑食", value: -2 },
-    { id: 1, title: "帮忙做家务", value: 3 },
-    { id: 1, title: "9点后完成作业", value: -2 },
-    { id: 1, title: "收拾玩具", value: 3 },
-    { id: 1, title: "收拾书", value: 2 },
-    { id: 1, title: "洗澡不积极", value: -2 },
-    { id: 1, title: "刷牙不积极", value: -2 },
-    { id: 1, title: "乱丢玩具", value: -3 },
-]
+
+// const tabs = ["aa", "bb", "cc"]
 
 const DiamondCard: React.FC<{}> = () => {
 
     const [result, setResult] = useState<Object[]>([]);
+
+    const [activeTab, setActiveTab] = useState<number>(0);
 
     useEffect(() => {
 
@@ -52,8 +35,6 @@ const DiamondCard: React.FC<{}> = () => {
     }
 
     const sum = () => {
-        console.log(result);
-
         const total = result.reduce((total, item) => {
             console.log(item);
             return total + item.value
@@ -63,7 +44,7 @@ const DiamondCard: React.FC<{}> = () => {
             title: '恭喜你获得',
             content: (
                 <div>
-                    <p>{total}</p>
+                    <p>{total} 张卡</p>
                 </div>
             ),
             onOk() { },
@@ -71,6 +52,8 @@ const DiamondCard: React.FC<{}> = () => {
     }
 
     const formatTitle = item => {
+        console.log(item);
+
         const { title, value } = item;
         if (value > 0) {
             return title + " +" + value;
@@ -79,34 +62,48 @@ const DiamondCard: React.FC<{}> = () => {
         }
     }
 
+    const formatValue = (value: number) => value > 0 ? " +" + value : " " + value
+
+
     const clean = () => {
         setResult([])
+    }
+
+    const onTabClick = id => {
+        setActiveTab(id)
     }
 
     return (
         <main className={styles.container}>
             <section className={styles.static}>
                 <ul>
-                    {staticData.map(item => <li key={item} className={item > 0 ? styles.add : styles.sub}
+                    {staticData.map(item => <li key={item} className={`${styles.button} ${item > 0 ? styles.add : styles.sub}`}
                         onClick={() => addStatic(item)}>{formatTitle({ title: "", value: item })}</li>)}
                 </ul>
             </section>
             <section className={styles.item}>
-                <ul>
-                    {itemList.filter(item=>item.value>0).map(item => <li key={item.id}
-                        className={`${item.value > 0 ? styles.add : styles.sub} ${result.filter(i => i.id == item.id).length > 0 ? styles.gray : ""}`}
-                        onClick={() => setResult([...result, item])}>{formatTitle(item)}</li>)}
-                </ul>
-                <hr/>
-                <ul>
-                    {itemList.filter(item=>item.value<0).map(item => <li key={item.id}
-                        className={`${item.value > 0 ? styles.add : styles.sub} ${result.filter(i => i.id == item.id).length > 0 ? styles.gray : ""}`}
-                        onClick={() => setResult([...result, item])}>{formatTitle(item)}</li>)}
-                </ul>
+                <div className={styles.neumorphicOuter}>
+                    <div className={styles.controls}>
+                        {tabs.map((tab, index) => <div key={tab} onClick={() => onTabClick(index)}
+                            className={`${styles.control} ${activeTab == index ? styles.active : ""}`}>{tab}</div>)}
+                    </div>
+                    <div className={styles.inner}>
+                        <ul>
+                            {(itemList[activeTab] || []).map(item => <li key={item.title}>
+                                <span className={result.filter(i => i.title == item.title).length > 0 ? styles.gray : ""}>{item.title} : </span>
+                                {item.values ? item.values.map(value => <small key={`${item.title}${value}`} onClick={() => setResult([...result, { ...item, value: value }])}
+                                    className={`${styles.button} ${value > 0 ? styles.add : styles.sub}`}
+                                >{formatValue(value)}</small>) : <small onClick={() => setResult([...result, { ...item, value: item.value }])}
+                                    className={`${styles.button} ${item.value > 0 ? styles.add : styles.sub}`}
+                                >{formatValue(item.value)}</small>}</li>)}
+                        </ul>
+                    </div >
+
+                </div >
             </section>
             <section className={styles.result}>
                 <ul>
-                    {result.map((item, index) => <li key={item.title + index} className={item.value > 0 ? styles.add : styles.sub}
+                    {result.map((item, index) => <li key={item.title + index} className={`${styles.button} ${item.value > 0 ? styles.add : styles.sub}`}
                         onClick={() => subItem(index)}>{formatTitle(item)}</li>)}
                 </ul>
             </section>
