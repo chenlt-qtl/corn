219ef97ba7666945a7bdd8358a2a8e82c137f037
chenlt <da.mu@163.com>
2023-08-03 17:04:41 +0800

    增加LOG

--- app/src/pages/read/index.tsx
+++ app/src/pages/read/index.tsx
@@ -6,159 +6,163 @@ import { doPlay } from '@/utils/wordUtils';
 import { MenuOutlined, LeftOutlined, RightOutlined, CaretRightOutlined } from '@ant-design/icons';
 
 const menuData = [
-  { ids: [32, 33, 34, 35], subTitle: 'at,an,ap,ad' },
-  { ids: [36, 37, 38, 39], subTitle: 'am,ag,ash,amp' },
-  { ids: [40, 41, 42, 43], subTitle: 'and,ack,ant,ed' },
-  { ids: [44, 45, 46, 47], subTitle: 'en,et,eck,ell' },
+    { ids: [32, 33, 34, 35], subTitle: 'at,an,ap,ad' },
+    { ids: [36, 37, 38, 39], subTitle: 'am,ag,ash,amp' },
+    { ids: [40, 41, 42, 43], subTitle: 'and,ack,ant,ed' },
+    { ids: [44, 45, 46, 47], subTitle: 'en,et,eck,ell' },
 ];
 
 const Read = (props, ref) => {
-  let { mId = 0 } = props.match.params;
+    let { mId = 0 } = props.match.params;
+    console.log(mId);
 
-  if (mId * 1 >= menuData.length) {
-    mId = 0;
-  }
 
-  const ids = menuData[mId].ids;
+    if (mId * 1 >= menuData.length) {
+        mId = 0;
+    }
 
-  const player = useRef();
-  const source = useRef();
+    const ids = menuData[mId * 1].ids;
 
-  const [picture, setPicture] = useState<String>();
-  const [positions, setPositions] = useState<[]>();
-  const [mp3Times, setMp3Times] = useState<[String]>();
-  const [loading, setLoading] = useState<boolean>(false);
-  const [activeIndex, setActiveIndex] = useState<number>(-1);
-  const [index, setIndex] = useState<number>(0);
+    const player = useRef();
+    const source = useRef();
 
-  useEffect(() => {
-    setIndex(0);
-    getData(0);
-  }, [mId]);
+    const [picture, setPicture] = useState<String>();
+    const [positions, setPositions] = useState<[]>();
+    const [mp3Times, setMp3Times] = useState<[String]>();
+    const [loading, setLoading] = useState<boolean>(false);
+    const [activeIndex, setActiveIndex] = useState<number>(-1);
+    const [index, setIndex] = useState<number>(0);
 
-  const getData = async (index: number) => {
-    setActiveIndex(-1);
-    setLoading(true);
-    const res = await getArticle(ids[index]);
-    const { article, sentences, read } = res.result;
-    source.current.src = article.mp3;
-    player.current.load();
+    useEffect(() => {
+        setIndex(0);
+        getData(0);
+    }, [mId]);
 
-    setPicture(article.picture);
+    const getData = async (index: number) => {
+        console.log("index", index);
 
-    setPositions((read.position || '').split('|'));
+        setActiveIndex(-1);
+        setLoading(true);
+        const res = await getArticle(ids[index]);
+        const { article, sentences, read } = res.result;
+        source.current.src = article.mp3;
+        player.current.load();
 
-    const mp3Times = [];
-    const { records } = sentences;
-    records.map((record) => mp3Times.push(record.mp3Time));
-    setMp3Times(mp3Times);
-    setLoading(false);
-  };
+        setPicture(article.picture);
 
-  const onPlay = (i) => {
-    setActiveIndex(i);
-    const times = mp3Times[i].split(',');
-    doPlay(player.current, times[0], times[1], 0.7);
-  };
+        setPositions((read.position || '').split('|'));
 
-  //菜单
-  const content = (
-    <div className={styles.menuContent}>
-      {menuData.map((menu, index) => (
-        <div
-          key={index}
-          onClick={() => onMenuClick(index)}
-          className={mId == index ? styles.activeMenu : ''}
-        >
-          {mId == index ? (
-            <div className={styles.caret}>
-              <CaretRightOutlined />
-            </div>
-          ) : (
-            ''
-          )}
-          <div className={styles.title}>M{index + 1}</div>
-          <div className={styles.subTitle}>{menu.subTitle}</div>
-        </div>
-      ))}
-    </div>
-  );
+        const mp3Times = [];
+        const { records } = sentences;
+        records.map((record) => mp3Times.push(record.mp3Time));
+        setMp3Times(mp3Times);
+        setLoading(false);
+    };
 
-  //菜单点击事件
-  const onMenuClick = (moduleId: number) => {
-    props.history.push('/all/read/' + moduleId);
-  };
+    const onPlay = (i) => {
+        setActiveIndex(i);
+        const times = mp3Times[i].split(',');
+        doPlay(player.current, times[0], times[1], 0.7);
+    };
 
-  //翻页
-  const go = (index: number) => {
-    setIndex(index);
-    getData(index);
-  };
-
-  const render = function () {
-    return (
-      <div className={styles.container}>
-        <audio ref={player}>
-          <source ref={source} type="audio/mpeg" />
-          您的浏览器不支持 audio 元素。
-        </audio>
-        <Spin spinning={loading}>
-          <div className={styles.content}>
-            <div className={styles.picture}>
-              {/*点读区域absolute*/}
-              {(mp3Times || []).map((i, index) => {
-                const positionArr = positions[index].split(',');
-                return (
-                  <div
+    //菜单
+    const content = (
+        <div className={styles.menuContent}>
+            {menuData.map((menu, index) => (
+                <div
                     key={index}
-                    onClick={() => onPlay(index)}
-                    className={`${styles.mask} ${index == activeIndex ? styles.active : ''}`}
-                    style={{ top: positionArr[0], height: positionArr[1] }}
-                  ></div>
-                );
-              })}
-
-              {/**真正有占空间的元素 */}
-              <img src={picture}></img>
-            </div>
-            {/*  */}
-          </div>
-        </Spin>
-        <div className={styles.toolbar}>
-          <div className={styles.btnDiv}>
-            <Popover content={content} trigger="click">
-              <Button type="primary" shape="circle" icon={<MenuOutlined />} />
-            </Popover>
-
-            <div className={styles.label}>目录</div>
-          </div>
-
-          <div className={styles.btnDiv}>
-            <Button
-              onClick={() => go(index - 1)}
-              type="primary"
-              shape="circle"
-              disabled={index == 0}
-              icon={<LeftOutlined />}
-            />
-            <div className={styles.label}>上一页</div>
-          </div>
-
-          <div className={styles.btnDiv}>
-            <Button
-              onClick={() => go(index + 1)}
-              type="primary"
-              shape="circle"
-              disabled={index >= ids.length - 1}
-              icon={<RightOutlined />}
-            />
-            <div className={styles.label}>下一页</div>
-          </div>
+                    onClick={() => onMenuClick(index)}
+                    className={mId == index ? styles.activeMenu : ''}
+                >
+                    {mId == index ? (
+                        <div className={styles.caret}>
+                            <CaretRightOutlined />
+                        </div>
+                    ) : (
+                        ''
+                    )}
+                    <div className={styles.title}>M{index + 1}</div>
+                    <div className={styles.subTitle}>{menu.subTitle}</div>
+                </div>
+            ))}
         </div>
-      </div>
     );
-  };
-  return render();
+
+    //菜单点击事件
+    const onMenuClick = (moduleId: number) => {
+        props.history.push('/all/read/' + moduleId);
+    };
+
+    //翻页
+    const go = (index: number) => {
+        setIndex(index);
+        getData(index);
+    };
+
+    const render = function () {
+        return (
+            <div className={styles.container}>
+                <audio ref={player}>
+                    <source ref={source} type="audio/mpeg" />
+                    您的浏览器不支持 audio 元素。
+                </audio>
+                <Spin spinning={loading}>
+                    <div className={styles.content}>
+                        <div className={styles.picture}>
+                            {/*点读区域absolute*/}
+                            {(mp3Times || []).map((i, index) => {
+                                const positionArr = positions[index].split(',');
+                                return (
+                                    <div
+                                        key={index}
+                                        onClick={() => onPlay(index)}
+                                        className={`${styles.mask} ${index == activeIndex ? styles.active : ''}`}
+                                        style={{ top: positionArr[0], height: positionArr[1] }}
+                                    ></div>
+                                );
+                            })}
+
+                            {/**真正有占空间的元素 */}
+                            <img src={picture}></img>
+                        </div>
+                        {/*  */}
+                    </div>
+                </Spin>
+                <div className={styles.toolbar}>
+                    <div className={styles.btnDiv}>
+                        <Popover content={content} trigger="click">
+                            <Button type="primary" shape="circle" icon={<MenuOutlined />} />
+                        </Popover>
+
+                        <div className={styles.label}>目录</div>
+                    </div>
+
+                    <div className={styles.btnDiv}>
+                        <Button
+                            onClick={() => go(index - 1)}
+                            type="primary"
+                            shape="circle"
+                            disabled={index == 0}
+                            icon={<LeftOutlined />}
+                        />
+                        <div className={styles.label}>上一页</div>
+                    </div>
+
+                    <div className={styles.btnDiv}>
+                        <Button
+                            onClick={() => go(index + 1)}
+                            type="primary"
+                            shape="circle"
+                            disabled={index >= ids.length - 1}
+                            icon={<RightOutlined />}
+                        />
+                        <div className={styles.label}>下一页</div>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+    return render();
 };
 
 export default Read;
