1ca9f1029a42149c5a4594779ac41611353bd0e0
chenlt <da.mu@163.com>
2023-08-16 15:31:22 +0800

    点击事件

--- app/src/components/Read/Chinese/index.tsx
+++ app/src/components/Read/Chinese/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './styles.less';
 import FaIcon from '@/components/FaIcon';
 
@@ -7,12 +7,22 @@ const Chinese = props => {
 
     const { chinese, visible, setVisible } = props;
 
+    const onShow = e => {
+        e.stopPropagation();
+        !visible && setVisible(true);
+    }
+
+    const onHide = e => {
+        e.stopPropagation();
+        setVisible(false);
+    }
+
     const render = function () {
 
         return (<>
             {chinese ? <div className={`${styles.chineseWin} ${visible ? styles.show : styles.hide}`} >
-                {visible ? <div className={styles.close} onClick={() => setVisible(false)} > <FaIcon className="fa-times-circle"></FaIcon></div> : null}
-                <div className={styles.chinese} onClick={() => !visible && setVisible(true)}>{visible ? chinese : "显示中文"}</div>
+                {visible ? <div className={styles.close} onClick={onHide} > <FaIcon className="fa-times-circle"></FaIcon></div> : null}
+                <div className={styles.chinese} onClick={onShow}>{visible ? chinese : "显示中文"}</div>
             </div> : null}
         </>
 
